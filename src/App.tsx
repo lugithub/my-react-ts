31d808bb7718e0393612f0e7372a741a8612834c
lubin <lu.bin.lu.bin@gmail.com>
2019-02-10 00:26:25 -0500

    P

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-class App extends Component {
+interface AppProp {
+  title: string;
+}
+
+class App extends Component<AppProp> {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
+          <p>{this.props.title}</p>
           <a
             className="App-link"
             href="https://reactjs.org"
