6eb6aa55645911b825ca3c4bdea252101a969e4c
lubin <lu.bin.lu.bin@gmail.com>
2019-02-10 12:23:21 -0500

    jest match snapshot example

--- src/ms-starter/components/__tests__/hello-h.test.tsx
+++ src/ms-starter/components/__tests__/hello-h.test.tsx
@@ -7,6 +7,11 @@ it("renders the correct text when no enthusiasm level is given", () => {
   expect(hello.find(".greeting").text()).toEqual("Hello Daniel!");
 });
 
+it("renders the correct text when no enthusiasm level is given, snapshot", () => {
+  const hello = shallow(<Helloh name="Daniel" />);
+  expect(hello).toMatchSnapshot();
+});
+
 it("renders the correct text with an explicit enthusiasm level of 2", () => {
   const hello = mount(<Helloh name="Daniel" />);
   hello.find(".hello").simulate("click");
