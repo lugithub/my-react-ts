a9e1042bedc0bb14c19690f15a54bbb04e786677
lubin <lu.bin.lu.bin@gmail.com>
2019-02-10 21:27:44 -0500

    enable redux dev tool

--- src/configure-store.ts
+++ src/configure-store.ts
@@ -1,8 +1,11 @@
-import { createStore } from "redux";
+import { createStore, compose } from "redux";
 
 import { rootReducer } from "./root-reducer";
 
 export default function configureStore() {
-  const store = createStore(rootReducer);
+  const composeEnhancers =
+    (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+  const store = createStore(rootReducer, composeEnhancers());
   return store;
 }
